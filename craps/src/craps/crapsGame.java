a90a745f5c16bb11c8d5f2ba171fb66d83274a71
danielle.failor <danielle.failor@MU-C1-02.mullen.pvt.k12.co.us>
2019-08-26 11:56:24 -0600

    changing to different methods

--- craps/src/craps/crapsGame.java
+++ craps/src/craps/crapsGame.java
@@ -2,63 +2,140 @@ package craps;
 import java.util.Scanner;
 public class crapsGame
 	{
-
-		public static void main(String[] args)
+		static String name;
+		static int account;
+		static boolean playing;
+		static Scanner userInput = new Scanner (System.in);
+		static Scanner userInput1 = new Scanner (System.in);
+		static Scanner userInput2 = new Scanner (System.in);
+		static int bet;
+		static boolean inRound;
+		static int dice1;
+		static int dice2;
+		static int dice3;
+		static int dice4;
+		static int totalDice;
+		static int totalDice2;
+		static boolean rolling;
+		static int answer;
+		public static void craps()
+		{
+			
+		}
+		public static void greetPlayer()
+		{
+			
+			System.out.println("Hello, What's your name?");
+			 name = userInput.nextLine();
+			 System.out.println("Hello " + name + " let's play Craps!");
+		}
+		public static void information()
 			{
-				Scanner userInput = new Scanner (System.in);
-				Scanner userInput1 = new Scanner (System.in);
-				Scanner userInput2 = new Scanner (System.in);
-				System.out.println("Hello, What's your name?");
-				String name = userInput.nextLine();
-				
-				System.out.println("Hello " + name + " let's play Craps!");
 				System.out.println("You will start off with $100. If you don't have any more money, the game will stop. ");
-				int account = 100;
-				boolean playing = true;
-				while(playing )
+				 account = 100;
+		}
+		public static void whilePlaying()
+		{
+			playing = true;
+			while(playing)
 				{
-				
-					System.out.println("How much will you be betting? You can't bet more than 100 a round.");
-					int bet = userInput1.nextInt();
-					if(account == 0) 
-					{
-						System.out.println("You have no more money and can't play anymore.");
-						playing = false;
-					}
-				
-				boolean inRound = true;
-				while (inRound)
+					askBet();
+					noMoney();
+					inTheRoundOfCraps();
+				}
+			
+		}
+		public static void askBet()
+		{
+			System.out.println("How much will you be betting? You can't bet more than 100 a round.");
+			 bet = userInput1.nextInt();
+		}
+		public static void noMoney()
+		{
+			if(account == 0) 
 				{
-				int dice1 = (int)(Math.random()*6+1);
-				int dice2 = (int)(Math.random()*6+1);
-				int totalDice = dice1 + dice2;
-				System.out.println("you rolled a "+ dice1+ " and " + dice2 + " equaling " + totalDice);
-				if (totalDice == 7 || totalDice==11)
-					{
-					 account = account + bet;
-						System.out.println("You rolled a "+ dice1+ " and a " + dice2+ " for a total of " + totalDice + ". You win, " + name + ". Do you want to play again? 1) yes 2) no. You have " + account + " in your account.");
-						
-						int answer0 = userInput2.nextInt();
-						if(answer0 == 1)
-						{
-							System.out.println("Ok. Have Fun!");
-							inRound = false;
-						
-							
-						}
-						else if (answer0 == 2)
-						{
-							System.out.println("Goodbye!");
-							playing = false;
-							inRound = false;
-							
-						}
-					}
-				else if( totalDice == 2 || totalDice == 12)
+					System.out.println("You have no more money and can't play anymore.");
+					playing = false;
+				}
+		}
+		public static void inTheRoundOfCraps()
+		{
+			 inRound = true;
+			 while (inRound)
+				 {
+					dice(); 
+					whatRolled();
+				 }
+				
+		}
+		public static void dice()
+		{
+			 dice1 = (int)(Math.random()*6+1);
+			 dice2 = (int)(Math.random()*6+1);
+			 totalDice = dice1 + dice2;
+		}
+		public static void whatRolled()
+		{
+			System.out.println("you rolled a "+ dice1+ " and " + dice2 + " for a total of " + totalDice);
+		}
+		public static void ifWonImmediately()
+		{
+			if (totalDice == 7 || totalDice==11)
+				{
+				 account = account + bet;
+					System.out.println("You win, " + name + ". Do you want to play again? 1) yes 2) no. You have " + account + " in your account.");
+					
+					
+		}
+		}
+		public static void ifLostImmediately()
+		{
+			 if( totalDice == 2 || totalDice == 12)
 					{
 					account = account - bet;
 					
-					System.out.println("You rolled a "+ dice1+ " and a " + dice2+ " for a total of " + totalDice + ". Sorry, but you lose, " + name + ". Do you want to play again? 1) yes 2) no. You have " + account + " in your account.");
+					System.out.println(" Sorry, but you lose, " + name + ". Do you want to play again? 1) yes 2) no. You have " + account + " in your account.");
+		}
+		}
+		public static void answer()
+		{
+			 answer = userInput2.nextInt();
+				if(answer == 1)
+				{
+					System.out.println("Ok. Have Fun!");
+					inRound = false;
+				
+					
+				}
+				else if (answer == 2)
+				{
+					System.out.println("Goodbye!");
+					playing = false;
+					inRound = false;
+					
+				}
+			}
+		
+		public static void main(String[] args)
+			{
+				
+				
+				
+				
+				 
+				while(playing )
+				{
+				
+					
+					
+				
+				
+				while (inRound)
+				{
+				
+				
+				
+				
 					int answer = userInput2.nextInt();
 					if(answer == 1)
 					{
@@ -75,15 +152,15 @@ public class crapsGame
 						
 					}
 					}
-				else 
+				else
 				{
-					boolean rolling = true;
+					 rolling = true;
 					while (rolling)
 					{
 					System.out.println("You rolled a "+ dice1+ " and a " + dice2+ " for a total of " + totalDice + ". ");
-					int dice3 = (int)(Math.random()*6+1);
-					int dice4 = (int)(Math.random()*6+1);
-					int totalDice2 = dice3 + dice4;
+					 dice3 = (int)(Math.random()*6+1);
+					 dice4 = (int)(Math.random()*6+1);
+					 totalDice2 = dice3 + dice4;
 					System.out.println("You rolled a " + dice3 + " and " + dice4 + " for a total of " + totalDice2 + ".");
 					if(totalDice2 ==7)
 					{
